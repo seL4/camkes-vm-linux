905d65dda2f5f2ad85111a0b426f7e44b6265dc9
Adam Felizzi <a.felizzi@student.unsw.edu.au>
2018-07-04 09:29:13 +1000

    CMake: Refactored linux-artifacts-helpers.cmake
    Broke up the helpers file into two seperate files.
    The downloading and configuring of linux is now to be performed
    at the 'top' level (outside of the external project). The external
    projects now expect the top level to pass in the downloaded
    linux source.

--- linux-source-helpers.cmake
+++ linux-source-helpers.cmake
@@ -12,8 +12,6 @@
 
 cmake_minimum_required(VERSION 3.8.2)
 
-set(VM_ARTIFACTS_DIR "${CMAKE_CURRENT_LIST_DIR}" CACHE INTERNAL "")
-
 # Function for getting the major version for the default Linux guest kernel provided
 # by the project
 # version: caller variable which is set to the kernel major version
@@ -51,23 +49,35 @@ function(DownloadLinux linux_major linux_minor linux_md5 linux_out_dir linux_out
     set(linux_dir "linux-${linux_version}")
     set(linux_archive "${linux_dir}.tar.gz")
     set(linux_url "https://www.kernel.org/pub/linux/kernel/v${linux_major}.x/${linux_archive}")
-    # Download the linux archive and verify its hash
-    if(NOT linux_md5 STREQUAL "")
-        file(DOWNLOAD ${linux_url} ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive}
-            SHOW_PROGRESS
-            EXPECTED_MD5 ${linux_md5})
-    else()
-        file(DOWNLOAD ${linux_url} ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive} SHOW_PROGRESS)
-    endif()
+    # Create a cmake file to download the linux source. We do this so we don't download the
+    # the linux source until build time
+    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/download_linux.cmake
+        "file(DOWNLOAD ${linux_url} ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive} \
+            SHOW_PROGRESS \
+            EXPECTED_MD5 ${linux_md5})"
+    )
+    # Custom command to execute linux source download
+    add_custom_command(OUTPUT out/${linux_archive}
+        COMMAND ${CMAKE_COMMAND} -P ${CMAKE_CURRENT_BINARY_DIR}/download_linux.cmake
+        VERBATIM
+    )
+    add_custom_target(download_${linux_out_target} DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive}")
     # Unpack linux tar archive
-    add_custom_command(OUTPUT out/${linux_dir}
+    # We track the .cmake_extracted file instead of the directory to avoid 'ninja clean' trying to delete
+    # the extracted directory
+    add_custom_command(OUTPUT out/${linux_dir}/.cmake_extracted
         COMMAND ${CMAKE_COMMAND} -E tar xf ${linux_archive}
+        COMMAND touch ${linux_dir}/.cmake_extracted
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/out
         VERBATIM
-        DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive}
+        DEPENDS download_${linux_out_target} ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive}
     )
     # Create custom target for tar extract
-    add_custom_target(extract_${linux_out_target} DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}")
+    add_custom_target(extract_${linux_out_target} DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}/.cmake_extracted")
+    # Add clean target for extracted linux directory.
+    add_custom_target(clean-${linux_out_target}
+        COMMAND bash -c "rm -r ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}"
+    )
     # Add linux out target
     add_custom_target(${linux_out_target} DEPENDS extract_${linux_out_target})
     set_property(TARGET ${linux_out_target} PROPERTY EXTRACT_DIR "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}")
@@ -85,7 +95,7 @@ function(ConfigureLinux linux_dir linux_config_location linux_symvers_location c
         "DEPENDS"
     )
     if (NOT "${CONFIGURE_LINUX_UNPARSED_ARGUMENTS}" STREQUAL "")
-        message(FATAL_ERROR "Unknown arguments to ConfigureLinux")
+        message(FATAL_ERROR "Unknown arguments to ConfigureLinux: ${CONFIGURE_LINUX_UNPARSED_ARGUMENTS}")
     endif()
     # Copy linux config & symvers
     add_custom_command(OUTPUT ${linux_dir}/.config ${linux_dir}/Module.symvers
@@ -109,59 +119,3 @@ function(ConfigureLinux linux_dir linux_config_location linux_symvers_location c
     )
     add_custom_target(${configure_linux_target} DEPENDS "${linux_dir}/.config.old")
 endfunction(ConfigureLinux)
-
-# Function to define a linux kernel module. Given the directory to the
-# kernel module
-# module_dir: Directory to linux kernel module
-function(DefineLinuxModule module_dir)
-    cmake_parse_arguments(PARSE_ARGV 1 DEFINE_LINUX_MODULE
-        ""
-        ""
-        "DEPENDS;INCLUDES")
-    # We require the linux source for compiling our kernel modules
-    if (NOT TARGET download_vm_linux)
-        # Use the linux md5 hash if its passed in
-        set(linux_md5 "")
-        if(LINUX_MD5)
-            set(linux_md5 "${LINUX_MD5}")
-        endif()
-        # Use the linux major and minor if its passed in
-        if(LINUX_MAJOR AND LINUX_MINOR)
-            set(linux_major ${LINUX_MAJOR})
-            set(linux_minor ${LINUX_MINOR})
-        else()
-            # Else default to a pre-defined major/minor
-            GetDefaultLinuxMajor(linux_major)
-            GetDefaultLinuxMinor(linux_minor)
-            GetDefaultLinuxMd5(linux_md5)
-        endif()
-        DownloadLinux(${linux_major} ${linux_minor} ${linux_md5} vm_linux_extract_dir download_vm_linux)
-        # Linux config and symvers are to be copied to unpacked archive
-        set(linux_config "${VM_ARTIFACTS_DIR}/linux_configs/${linux_major}.${linux_minor}/config")
-        set(linux_symvers "${VM_ARTIFACTS_DIR}/linux_configs/${linux_major}.${linux_minor}/Module.symvers")
-        ConfigureLinux(${vm_linux_extract_dir} ${linux_config} ${linux_symvers} configure_vm_linux
-            DEPENDS download_vm_linux
-        )
-    endif()
-    # Get linux extract directory (Kbuild directory)
-    get_target_property(linux_extract_dir download_vm_linux EXTRACT_DIR)
-    get_filename_component(module_name ${module_dir} NAME)
-    # Build Linux Module
-    set(module_includes "")
-    foreach(inc IN LISTS DEFINE_LINUX_MODULE_INCLUDES)
-        if(module_includes STREQUAL "")
-            set(module_includes "-I${inc}")
-        else()
-            set(module_includes "${module_includes} -I${inc}")
-        endif()
-    endforeach()
-    add_custom_command(OUTPUT ${module_name}.ko
-        COMMAND bash -c "make MODULE_INCLUDES=\"${module_includes}\" KHEAD=${linux_extract_dir}"
-        COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_CURRENT_SOURCE_DIR}/${module_dir}/${module_name}.ko" "${CMAKE_CURRENT_BINARY_DIR}/${module_name}.ko"
-        VERBATIM
-        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${module_dir}
-        DEPENDS ${DEFINE_LINUX_MODULE_DEPENDS} download_vm_linux configure_vm_linux
-    )
-    # Add target for linux module
-    add_custom_target(build_module_${module_name} ALL DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/${module_name}.ko")
-endfunction(DefineLinuxModule)
