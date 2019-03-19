b659c57eb2ab3aa275494028843c656b39ea940c
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2019-03-19 16:16:05 +1100

    tools: Updated printing in artifacts script
    Use printf over echo -e + Use tputs instead of hardcoded
    escape sequences.

--- tools/rootfs_unpack/install_artifacts_rootfs.sh
+++ tools/rootfs_unpack/install_artifacts_rootfs.sh
@@ -18,26 +18,26 @@ OUTPUT_DIR=out
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 usage() {
-    echo "Usage ./install_artifacts_rootfs.sh --mode=[overlay|fs_install|rootfs_install] --image=<path to rootfs/initrd file> \
+    printf "Usage ./install_artifacts_rootfs.sh --mode=[overlay|fs_install|rootfs_install] --image=<path to rootfs/initrd file> \
 --distro=[debian|buildroot] --root-install=<location of artifacts to install in rootfs> \
 [--splitgz | --custom-init=<path to custom root init script> --output-image-name=<name of output image> --output-dir=<directory to output files to>]"
     exit 1
 }
 
 error_exit() {
-    echo -e "$1">&2
+    printf "$1\n">&2
     exit 1
 }
 
 gzip_rootfs_cpio() {
-    echo -e "\e[1;96mGzipping rootfs cpio image\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Gzipping rootfs cpio image$(tput sgr0)\n"
     pushd ${OUTPUT_DIR}
     gzip -k ${OUTPUT_NAME}
     popd
 }
 
 split_initrd() {
-    echo -e "\e[1;96mExtracting gz image from initrd\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Extracting gz image from initrd$(tput sgr0)\n"
     start=`grep -a -b -m 1 --only-matching '070701' $1 | head -1 | cut -f 1 -d :`
     end=`grep -a -b -m 1 --only-matching 'TRAILER!!!' $1 | head -1 | cut -f 1 -d :`
     # We assume the rootfs gzip file is found after the trailer
@@ -46,63 +46,63 @@ split_initrd() {
     bs=$((end + 512 - (end % 512)))
     dd if=$1 bs=${bs} skip=1 of=${OUTPUT_DIR}/${filename}.gz
     dd if=$1 of=${OUTPUT_DIR}/header.cpio count=${bs} ibs=1
-    gunzip -f ${OUTPUT_DIR}/${filename}.gz || error_exit "\e[0;31mUnable to unzip gz image\e[0m"
+    gunzip -f ${OUTPUT_DIR}/${filename}.gz || error_exit "$(tput setaf 1)$(tput bold)Unable to unzip gz image$(tput sgr0)\n"
 }
 
 unpack_rootfs_cpio() {
-    echo -e "\e[1;96mUnpacking rootfs cpio image: $1\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Unpacking rootfs cpio image: $1$(tput sgr0)\n"
     UNPACK_FILE=$1
     if [[ "$UNPACK_FILE" = *.gz ]]; then
         GUNZIP_FILE=${UNPACK_FILE##*/}
         gunzip -c ${UNPACK_FILE} > ${OUTPUT_DIR}/${GUNZIP_FILE::-3}
         UNPACK_FILE="../${GUNZIP_FILE%.gz}"
-        echo -e "\e[1;96mUnzipped gz image to ${GUNZIP_FILE}\e[0m"
+        printf "$(tput setaf 6)$(tput bold)Unzipped gz image to ${GUNZIP_FILE}$(tput sgr0)\n"
     fi
     mkdir -p ${OUTPUT_DIR}/unpack
     pushd ${OUTPUT_DIR}/unpack
-    fakeroot cpio -id --no-preserve-owner --preserve-modification-time < ${UNPACK_FILE} || error_exit "\e[0;31mUnpacking CPIO failed\e[0m"
+    fakeroot cpio -id --no-preserve-owner --preserve-modification-time < ${UNPACK_FILE} || error_exit "$(tput setaf 1)$(tput bold)Unpacking CPIO failed$(tput sgr0)\n"
     popd
 }
 
 repack_rootfs_cpio() {
-    echo -e "\e[1;96mRepacking rootfs cpio image\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Repacking rootfs cpio image$(tput sgr0)\n"
     pushd ${OUTPUT_DIR}/unpack
     find . -print0 | fakeroot cpio --null -H newc -o > ../${OUTPUT_NAME}
-    echo -e "\e[1;96mCleaning unpack directory: ${OUTPUT_DIR}/unpack\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Cleaning unpack directory: ${OUTPUT_DIR}/unpack$(tput sgr0)\n"
     rm -r ${OUTPUT_DIR}/unpack
     popd
 }
 
 build_initrd() {
-    echo -e "\e[1;96mRebuild initrd image\e[0m"
-    gzip ${OUTPUT_DIR}/${OUTPUT_NAME} || error_exit "\e[0;31mUnable to gzip cpio image\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Rebuild initrd image$(tput sgr0)\n"
+    gzip ${OUTPUT_DIR}/${OUTPUT_NAME} || error_exit "$(tput setaf 1)$(tput bold)Unable to gzip cpio image$(tput sgr0)\n"
     cat ${OUTPUT_DIR}/header.cpio ${OUTPUT_DIR}/${OUTPUT_NAME}.gz > ${OUTPUT_DIR}/${OUTPUT_NAME}
 }
 
 debian_setup() {
-    echo -e "\e[1;96mConfiguring Debian/Ubuntu rootfs\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Configuring Debian/Ubuntu rootfs$(tput sgr0)\n"
     if [ $MODE == "rootfs_install" ]; then
         install_rootfs_artifacts
     elif [ $MODE == "overlay" ]; then
         install_fs_artifacts
-        cp ${SCRIPT_DIR}/init_install_scripts/debian/overlay ${OUTPUT_DIR}/unpack/scripts/overlay || error_exit "\e[0;31mInstalling overlay failed\e[0m"
+        cp ${SCRIPT_DIR}/init_install_scripts/debian/overlay ${OUTPUT_DIR}/unpack/scripts/overlay || error_exit "$(tput setaf 1)$(tput bold)Installing overlay failed$(tput sgr0)\n"
     else
         install_fs_artifacts
-        cp ${SCRIPT_DIR}/init_install_scripts/debian/fs_install ${OUTPUT_DIR}/unpack/scripts/fs_install || error_exit "\e[0;31mInstalling fs_install failed\e[0m"
+        cp ${SCRIPT_DIR}/init_install_scripts/debian/fs_install ${OUTPUT_DIR}/unpack/scripts/fs_install || error_exit "$(tput setaf 1)$(tput bold)Installing fs_install failed$(tput sgr0)\n"
     fi
 }
 
 buildroot_setup() {
-    echo -e "\e[1;96mConfiguring Buildroot rootfs\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Configuring Buildroot rootfs$(tput sgr0)\n"
     if  [ "$MODE" != "rootfs_install" ]; then
-        echo "${MODE} is unsupported for buildroot. Only 'rootfs_install' is supported"
+        printf "${MODE} is unsupported for buildroot. Only 'rootfs_install' is supported"
         exit 1
     fi
     install_rootfs_artifacts
 }
 
 install_fs_artifacts() {
-    echo -e "\e[1;96mInstalling build artifacts\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Installing build artifacts$(tput sgr0)\n"
     mkdir -p ${OUTPUT_DIR}/unpack/sel4_vm_artifacts
     if [[ -d $ROOT_INSTALL ]]; then
         cp -r ${ROOT_INSTALL}/* ${OUTPUT_DIR}/unpack/sel4_vm_artifacts/.
@@ -115,9 +115,9 @@ install_fs_artifacts() {
 }
 
 install_rootfs_artifacts() {
-    echo -e "\e[1;96mInstalling/Syncing build artifacts into rootfs\e[0m"
+    printf "$(tput setaf 6)$(tput bold)Installing/Syncing build artifacts into rootfs$(tput sgr0)\n"
     if [[ ! -d $ROOT_INSTALL ]]; then
-        echo "root-install needs to be a directory"
+        printf "root-install needs to be a directory"
     fi
     rsync -a ${ROOT_INSTALL}/ ${OUTPUT_DIR}/unpack
 }
@@ -128,7 +128,7 @@ do
         -m=*|--mode=*)
         MODE="${arg#*=}"
         if [ "$MODE" != "overlay" ] && [ "$MODE" != "fs_install" ] && [ "$MODE" != "rootfs_install" ]; then
-            echo "Mode '${arg#*=}' is not supported"
+            printf "Mode '${arg#*=}' is not supported\n"
             usage
         fi
         shift
@@ -144,7 +144,7 @@ do
         -d=*|--distro=*)
         DISTRO="${arg#*=}"
         if [ "$DISTRO" != "debian" ] && [ "$DISTRO" != "buildroot" ]; then
-            echo "Distro '${arg#*=}' is not supported"
+            printf "Distro '${arg#*=}' is not supported\n"
             usage
         fi
         shift
@@ -170,7 +170,7 @@ do
         shift
         ;;
         *)
-            echo "Unknown argument: ${arg}"
+            printf "Unknown argument: ${arg}\n"
             usage
         ;;
     esac
@@ -180,10 +180,10 @@ if [ -z "$MODE" ] || [ -z "$IMAGE" ] || [ -z "$DISTRO" ] || [ -z "$ROOT_INSTALL"
     usage
 fi
 
-echo "MODE=${MODE}"
-echo "IMAGE=${IMAGE}"
-echo "DISTRO=${DISTRO}"
-echo "CUSTOM-INIT=${CUSTOM_INIT}"
+printf "MODE=${MODE}\n"
+printf "IMAGE=${IMAGE}\n"
+printf "DISTRO=${DISTRO}\n"
+printf "CUSTOM-INIT=${CUSTOM_INIT}\n"
 
 mkdir -p ${OUTPUT_DIR}
 
@@ -214,4 +214,4 @@ fi
 
 OUTPUT=`pwd`/${OUTPUT_DIR}/rootfs_out.cpio
 
-echo ${OUTPUT}
+printf ${OUTPUT}
