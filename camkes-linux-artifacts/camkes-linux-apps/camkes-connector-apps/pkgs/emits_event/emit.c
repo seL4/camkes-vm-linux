34e99cf038b54bee55a69b730f8e93a8ad2bc600
Alison Felizzi <Alison.Felizzi@data61.csiro.au>
2019-11-08 14:35:58 +1100

    connector-apps: Updated emits event package
    Updated the initialisation and usage of the camkes event emit
    functionality. The emits event app now access event connections
    through userspace io established by our connections PCI driver.
    This involves writing to a register on the event bar of the
    connection device to signal an event.

--- camkes-linux-artifacts/camkes-linux-apps/camkes-connector-apps/pkgs/emits_event/emit.c
+++ camkes-linux-artifacts/camkes-linux-apps/camkes-connector-apps/pkgs/emits_event/emit.c
@@ -14,28 +14,38 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
+#include <unistd.h>
+#include <assert.h>
 
+#include <sys/types.h>
 #include <sys/stat.h>
-
-#include "emits_event.h"
+#include <sys/mman.h>
 
 int main(int argc, char *argv[]) {
 
     if (argc != 2) {
-        printf("Usage: %s eventfile\n", argv[0]);
+        printf("Usage: %s file\n\n"
+               "Emits an event on a given device connection\n",
+               argv[0]);
         return 1;
     }
 
-    char *filename = argv[1];
+    char *connection_name = argv[1];
 
-    int fd = open(filename, O_RDWR);
+    int fd = open(connection_name, O_RDWR);
+    assert(fd >= 0);
 
-    int error = emits_event_emit(fd);
-
-    if (error) {
-        printf("Error: %s\n", strerror(errno));
-        return -1;
+    char *connection;
+    if ((connection = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0 * getpagesize())) == (void *) -1) {
+        printf("mmap failed\n");
+        close(fd);
     }
+
+    /* Write at register address 0 to trigger an emit signal */
+    connection[0] = 1;
+
+    munmap(connection, 0x1000);
+    close(fd);
 
     return 0;
 }
