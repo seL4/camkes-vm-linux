122562b460110fb7134d78203d84c2081f57cb49
Alison Felizzi <Alison.Felizzi@data61.csiro.au>
2020-01-08 14:44:30 +1100

    connector-apps: Updated string reverse package
    Updated the initialisation and usage of the camkes connector
    functionality. The string reverse app now accesses dataports
    and event connections through userspace io established by our
    connections PCI driver.

--- camkes-linux-artifacts/camkes-linux-apps/camkes-connector-apps/pkgs/string_reverse/string_reverse.c
+++ camkes-linux-artifacts/camkes-linux-apps/camkes-connector-apps/pkgs/string_reverse/string_reverse.c
@@ -15,39 +15,43 @@
 #include <unistd.h>
 #include <string.h>
 #include <assert.h>
+#include <stdlib.h>
 
 #include <sys/stat.h>
 #include <sys/mman.h>
 
-#include "consumes_event.h"
-#include "emits_event.h"
-
-#define READY "/dev/camkes_reverse_ready"
-#define DONE "/dev/camkes_reverse_done"
-#define DATAPORTS_NAME "/dev/uio0"
+#define READY "/dev/uio0"
+#define DONE "/dev/uio1"
 
 #define BUFSIZE 2048
 char buf[BUFSIZE];
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
     int ready = open(READY, O_RDWR);
     int done = open(DONE, O_RDWR);
 
-    int dataport_fd = open(DATAPORTS_NAME, O_RDWR);
-    assert(dataport_fd >= 0);
-
-    volatile char *src_data;
-    if ((src_data = mmap(NULL, BUFSIZE, PROT_READ|PROT_WRITE, MAP_SHARED, dataport_fd, 0 * getpagesize())) == (__caddr_t)-1) {
-        printf("mmap src (index 0) failed\n");
-        close(dataport_fd);
+    char *src_data;
+    if ((src_data = mmap(NULL, BUFSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, ready, 1 * getpagesize())) == (void *) -1) {
+        printf("mmap src (index 1) failed\n");
+        close(ready);
+        close(done);
         return -1;
     }
-    volatile char *dest_data;
-    if ((dest_data = mmap(NULL, BUFSIZE, PROT_READ|PROT_WRITE, MAP_SHARED, dataport_fd, 1 * getpagesize())) == (__caddr_t)-1) {
+    char *emit;
+    if ((emit = mmap(NULL, 0x1000, PROT_READ | PROT_WRITE, MAP_SHARED, ready, 0 * getpagesize())) == (void *) -1) {
+        printf("mmap failed\n");
+        close(ready);
+        close(done);
+    }
+
+    char *dest_data;
+    if ((dest_data = mmap(NULL, BUFSIZE, PROT_READ | PROT_WRITE, MAP_SHARED, done, 1 * getpagesize())) == (void *) -1) {
         printf("mmap dest (index 1) failed\n");
         munmap(src_data, BUFSIZE);
-        close(dataport_fd);
+        close(ready);
+        close(done);
         return -1;
     }
 
@@ -57,33 +61,39 @@ int main(int argc, char *argv[]) {
             buf[last_idx] = '\0';
         }
 
-        // copy buf to src_data
+        /* copy buf to src_data */
         int i;
         for (i = 0; buf[i]; i++) {
             src_data[i] = buf[i];
         }
         src_data[i] = '\0';
 
-        // signal that we're ready for camkes component to reverse string
-        int error = emits_event_emit(ready);
-        assert(error == 0);
+        /* Signal that we're ready for camkes component to reverse string by writing
+         * at register address 0 to trigger an emit signal */
+        emit[0] = 1;
 
-        // wait for camkes to signal that the string is reversed
-        error = consumes_event_wait(done);
-        assert(error > 0);
+        /* wait for camkes to signal that the string is reversed */
+        int val;
+        int result = read(done, &val, sizeof(val));
 
+        if (result < 0) {
+            printf("Error: %s\n", strerror(result));
+            munmap(src_data, BUFSIZE);
+            munmap(dest_data, BUFSIZE);
+            close(ready);
+            close(done);
+            return -1;
+        }
         // read the result out of dest_data
-        strncpy(buf, (char*)dest_data, BUFSIZE);
+        strncpy(buf, (char *)dest_data, BUFSIZE);
 
         printf("%s\n", buf);
     }
 
-    close(ready);
-    close(done);
-    close(dataport_fd);
-
     munmap(src_data, BUFSIZE);
     munmap(dest_data, BUFSIZE);
+    close(ready);
+    close(done);
 
     return 0;
 }
