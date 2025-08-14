0895d3454b6ecc62fdb62a63e77b7e91f8fdc5c1
Gerwin Klein <gerwin.klein@proofcraft.systems>
2025-08-14 17:50:56 +0100

    trivial: update style for new cmake-format
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- linux-source-helpers.cmake
+++ linux-source-helpers.cmake
@@ -10,21 +10,30 @@ cmake_minimum_required(VERSION 3.16.0)
 # by the project
 # version: caller variable which is set to the kernel major version
 function(GetDefaultLinuxMajor version)
-    set(${version} "4" PARENT_SCOPE)
+    set(${version}
+        "4"
+        PARENT_SCOPE
+    )
 endfunction(GetDefaultLinuxMajor)
 
 # Function for getting the minor version for the default Linux guest kernel provided
 # by the project
 # version: caller variable which is set to the kernel minor version
 function(GetDefaultLinuxMinor version)
-    set(${version} "8.16" PARENT_SCOPE)
+    set(${version}
+        "8.16"
+        PARENT_SCOPE
+    )
 endfunction(GetDefaultLinuxMinor)
 
 # Function for getting the md5 hash for the default Linux guest kernel provided
 # by the project
 # md5: caller variable which is set to the kernel md5
 function(GetDefaultLinuxMd5 md5)
-    set(${md5} "5230b0185b5f4916feab86c450207606" PARENT_SCOPE)
+    set(${md5}
+        "5230b0185b5f4916feab86c450207606"
+        PARENT_SCOPE
+    )
 endfunction(GetDefaultLinuxMd5)
 
 # Function for getting the version for the default Linux guest kernel provided
@@ -33,7 +42,10 @@ endfunction(GetDefaultLinuxMd5)
 function(GetDefaultLinuxVersion version)
     GetDefaultLinuxMinor(minor)
     GetDefaultLinuxMajor(major)
-    set(${version} "${major}.${minor}" PARENT_SCOPE)
+    set(${version}
+        "${major}.${minor}"
+        PARENT_SCOPE
+    )
 endfunction(GetDefaultLinuxVersion)
 
 # Function for downloading the Linux source inorder to build kernel modules
@@ -46,9 +58,8 @@ function(DownloadLinux linux_major linux_minor linux_md5 linux_out_dir linux_out
     # Create a cmake file to download the linux source. We do this so we don't download the
     # the linux source until build time
     file(
-        WRITE
-            ${CMAKE_CURRENT_BINARY_DIR}/download_linux.cmake
-            "file(DOWNLOAD ${linux_url} ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive} \
+        WRITE ${CMAKE_CURRENT_BINARY_DIR}/download_linux.cmake
+        "file(DOWNLOAD ${linux_url} ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive} \
             SHOW_PROGRESS \
             EXPECTED_MD5 ${linux_md5})"
     )
@@ -59,16 +70,14 @@ function(DownloadLinux linux_major linux_minor linux_md5 linux_out_dir linux_out
         VERBATIM
     )
     add_custom_target(
-        download_${linux_out_target}
-        DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive}"
+        download_${linux_out_target} DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_archive}"
     )
     # Unpack linux tar archive
     # We track the .cmake_extracted file instead of the directory to avoid 'ninja clean' trying to delete
     # the extracted directory
     add_custom_command(
         OUTPUT out/${linux_dir}/.cmake_extracted
-        COMMAND
-            ${CMAKE_COMMAND} -E tar xf ${linux_archive}
+        COMMAND ${CMAKE_COMMAND} -E tar xf ${linux_archive}
         COMMAND touch ${linux_dir}/.cmake_extracted
         WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/out
         VERBATIM
@@ -81,27 +90,26 @@ function(DownloadLinux linux_major linux_minor linux_md5 linux_out_dir linux_out
     )
     # Add clean target for extracted linux directory.
     add_custom_target(
-        clean-${linux_out_target}
-        COMMAND bash -c "rm -r ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}"
+        clean-${linux_out_target} COMMAND bash -c
+                                          "rm -r ${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}"
     )
     # Add linux out target
     add_custom_target(${linux_out_target} DEPENDS extract_${linux_out_target})
     set_property(
-        TARGET ${linux_out_target}
-        PROPERTY EXTRACT_DIR "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}"
+        TARGET ${linux_out_target} PROPERTY EXTRACT_DIR
+                                            "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}"
     )
     # Pass out directory back to parent
-    set(${linux_out_dir} "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}" PARENT_SCOPE)
+    set(${linux_out_dir}
+        "${CMAKE_CURRENT_BINARY_DIR}/out/${linux_dir}"
+        PARENT_SCOPE
+    )
 endfunction(DownloadLinux)
 
 # Function for configuring the Linux source located at 'linux_dir' with given
 # config (linux_config_location) and symvers (linux_symvers_location)
-function(
-    ConfigureLinux
-    linux_dir
-    linux_config_location
-    linux_symvers_location
-    configure_linux_target
+function(ConfigureLinux linux_dir linux_config_location linux_symvers_location
+         configure_linux_target
 )
     # Get any existing dependencies for configuring linux
     cmake_parse_arguments(PARSE_ARGV 4 CONFIGURE_LINUX "" "ARCH;CROSS_COMPILE" "DEPENDS")
@@ -121,17 +129,15 @@ function(
     # Copy linux config & symvers
     add_custom_command(
         OUTPUT ${linux_dir}/.config ${linux_dir}/Module.symvers
-        COMMAND
-            ${CMAKE_COMMAND} -E copy "${linux_config_location}" "${linux_dir}/.config"
-        COMMAND
-            ${CMAKE_COMMAND} -E copy "${linux_symvers_location}" "${linux_dir}/Module.symvers"
+        COMMAND ${CMAKE_COMMAND} -E copy "${linux_config_location}" "${linux_dir}/.config"
+        COMMAND ${CMAKE_COMMAND} -E copy "${linux_symvers_location}" "${linux_dir}/Module.symvers"
         VERBATIM
         DEPENDS ${CONFIGURE_LINUX_DEPENDS}
     )
     # Create copy config target
     add_custom_target(
-        ${configure_linux_target}_copy_configs
-        DEPENDS "${linux_dir}/.config" "${linux_dir}/Module.symvers"
+        ${configure_linux_target}_copy_configs DEPENDS "${linux_dir}/.config"
+                                                       "${linux_dir}/Module.symvers"
     )
     # Prepare/Configure Linux Build Directory
     add_custom_command(
